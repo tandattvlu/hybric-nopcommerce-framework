4b2b55b09ceddf2a94741b030992ca8f2a16dc29
Phan Tan Dat <kukulele734@gmail.com>
2025-12-12 19:28:06 +0700

    Finish CustomerInfoPageUI

--- interfaces/pageUIs/HomePageUI.java
+++ interfaces/pageUIs/HomePageUI.java
@@ -1,5 +1,9 @@
 package pageUIs;
 
 public class HomePageUI {
+    public static final String REGISTER_LINK = "//a[@class='ico-register']";
+
+    public static final String MY_ACCOUNT_LINK = "//a[@class='ico-account']']";
+
 
 }
